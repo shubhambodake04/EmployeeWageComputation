93e1bf028c591db92b11814673ab135792b9bcda
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-24 13:53:34 +0530

    Added into master

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,17 +8,23 @@ namespace EmployeeWageComputation
         {
             Console.WriteLine("Welcome To Employee Wages Computation ");
             int fullTime = 1;
+            int partTime = 2;
             int empRate = 20;
 
             int empHrs = 0;
+            int empPartHrs = 0;
             int empWage = 1;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == fullTime)
                 empHrs = 8;
+            else if (empCheck == partTime)
+                empPartHrs = 4;
 
             empWage = empHrs * empRate;
-            Console.WriteLine("Emp Wage: " + empWage);
+            empPartHrs = empPartHrs * empRate;
+            Console.WriteLine("Emp Full Time Wage: " + empWage);
+            Console.WriteLine("Emp part Time Wage:" + empPartHrs);
             Console.ReadKey();
         }
     }
