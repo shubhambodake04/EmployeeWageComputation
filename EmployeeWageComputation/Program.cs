996cae0dd6346b3383282b0a588216b44eab72f3
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-24 14:12:40 +0530

    Added in master

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,31 +7,38 @@ namespace EmployeeWageComputation
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAYS = 20;
 
         static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NO_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
 
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
 
-                default:
-                    empHrs = 0;
-                    break;
+                    default:
+                        empHrs = 0;
+                        break;
 
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage:" + empWage);
             }
-
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage:" + empWage);
+            Console.WriteLine("Total Emp Wage:" + totalEmpWage);
         }
     }
 }
