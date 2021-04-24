b677196cbdb848a4fa1c977ec9c87bad3372fd9d
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-24 13:45:09 +0530

    Added in master

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,13 +6,19 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Welcome To Employee Wages Computation ");
+            int fullTime = 1;
+            int empRate = 20;
+
+            int empHrs = 0;
+            int empWage = 1;
             Random random = new Random();
-            int check = random.Next(0, 2);
-            if (check == 0)
-                Console.WriteLine("Employe is Present");
-            else
-                Console.WriteLine("Employee is Absent");
+            int empCheck = random.Next(0, 2);
+            if (empCheck == fullTime)
+                empHrs = 8;
+
+            empWage = empHrs * empRate;
+            Console.WriteLine("Emp Wage: " + empWage);
             Console.ReadKey();
         }
     }
