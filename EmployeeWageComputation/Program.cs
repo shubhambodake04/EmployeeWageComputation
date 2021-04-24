c6b601320d1bd74c6cdf804121da464481fdc0d3
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-24 13:36:37 +0530

    Added in master

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,14 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == 0)
+                Console.WriteLine("Employe is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+            Console.ReadKey();
         }
     }
 }
